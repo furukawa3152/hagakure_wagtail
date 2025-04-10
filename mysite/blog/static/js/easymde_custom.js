8698600a715e83b784d8a33944c0f3b5737063de
okuda1298 <okuda1298@gmail.com>
2025-04-10 23:17:27 +0900

    マークダウンエディタにコードボタン追加

---
+++ mysite/blog/static/js/easymde_custom.js
@@ -0,0 +1,10 @@
+window.wagtailMarkdown = window.wagtailMarkdown || {};
+window.wagtailMarkdown.options = window.wagtailMarkdown.options || {};
+window.wagtailMarkdown.options.tabSize = 4;
+window.wagtailMarkdown.options.toolbar = [
+    "bold", "italic", "heading", "|",
+    "code", "quote", "unordered-list", "ordered-list", "|",
+    "link", "|",
+    "preview", "side-by-side", "fullscreen", "|",
+    "guide"
+];
--- mysite/blog/wagtail_hooks.py
+++ mysite/blog/wagtail_hooks.py
@@ -3,6 +3,9 @@ from wagtail.admin.menu import MenuItem
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from wagtail.models import Page
+from django.templatetags.static import static
+from django.utils.html import format_html
+
 
 @hooks.register("construct_explorer_page_queryset")
 def restrict_editor_subpages(parent_page, queryset, request):
@@ -10,3 +13,9 @@ def restrict_editor_subpages(parent_page, queryset, request):
     if request.user.groups.filter(name="Editor").exists():
         return queryset.filter(content_type__model="blogpage")
     return queryset  # それ以外のユーザーはそのまま
+
+
+@hooks.register("insert_global_admin_js", order=100)
+def global_admin_js():
+    """Add /static/js/admin/easymde_custom.js to the admin."""
+    return format_html('<script src="{}"></script>', static("js/easymde_custom.js"))
